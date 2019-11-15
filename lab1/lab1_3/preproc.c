cee8f3bb38b412ddbeb4c0a2e07af216e5bc92a8
Anton S <soderlund.anton@gmail.com>
2019-11-15 09:38:31 +0000

    lab1_2 done lab1_3 started

---
+++ lab1/lab1_3/preproc.c
@@ -0,0 +1,21 @@
+
+int factorial(int n) {
+  if (n == 0)
+    return 1;
+  else
+    return n * factorial(n - 1);
+}
+
+int plus(int x, int y) {
+  return x + y;
+}
+
+#define PLUS(x, y) ((x) + (y))
+
+
+int main(){
+	int val;
+	val =factorial(n);
+
+	return 0;
+}
