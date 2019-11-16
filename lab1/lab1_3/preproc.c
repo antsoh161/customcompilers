53bffc6df4ce1130539c7f8bdc04782e9400178b
Anton S <soderlund.anton@gmail.com>
2019-11-16 06:53:03 +0000

    done

--- lab1/lab1_3/preproc.c
+++ lab1/lab1_3/preproc.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#define PLUS(x, y) ((x) + (y))
+//#define FACT(n) ( n==0 ? 1 : FACT(n-1)*n) fungerar inte
+
 
 int factorial(int n) {
   if (n == 0)
@@ -10,12 +14,10 @@ int plus(int x, int y) {
   return x + y;
 }
 
-#define PLUS(x, y) ((x) + (y))
-
-
 int main(){
 	int val;
-	val =factorial(n);
-
+	int n = 4;
+	val = FACT(n);
+	printf("%d\n",val);
 	return 0;
 }
