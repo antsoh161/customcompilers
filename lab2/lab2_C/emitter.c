ea7a617c426ae30270da28c2e19374dcc2e71a65
Anton S <soderlund.anton@gmail.com>
2019-11-21 17:08:32 +0100

    lab2_C done

---
+++ lab2/lab2_C/emitter.c
@@ -0,0 +1,42 @@
+/* emitter.c */
+
+#include "global.h"
+
+int calc(int token_type){
+    int op1 = pop();
+    int op2 = pop();
+    switch(token_type){
+        case '+' :
+            return op1+op2;
+        case '-' :
+            return op1-op2;
+        case '*' : 
+            return op1*op2;
+        case '/' : 
+            return op1/op2;
+        default:
+            printf("[Unknown token %d, with value %d]\n", token_type, token_value);
+            return 0;
+    }
+}
+
+void emit (int token_type, int token_value)  /*  generates output  */
+{
+    switch(token_type) {
+        case '+' : case '-' : case '*' : case '/' :
+            printf("%c\n",token_type); push(NUM,calc(token_type)); break;
+        case NUM : 
+            printf("%d\n",token_value); push(token_type, token_value);break;
+        case ID : 
+            printf("%s\n",symtable[token_value].lexeme); push(token_type,token_value);break;
+        case '=' :
+            printf("%c\n",token_type);
+            int op1 = pop();
+            int op2 = pop();
+            symtable[op2].value = op1;
+            printf("result: %d\n",op1);break;
+            
+    default:     
+        printf("[Unknown token %d, with value %d]\n", token_type, token_value);
+    }
+}
