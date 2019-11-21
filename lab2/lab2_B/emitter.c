d9eb48dd8f7a1230f7b2c005b3b457fd6eacfef9
Anton S <soderlund.anton@gmail.com>
2019-11-21 14:09:51 +0100

    part b done

--- lab2/lab2_B/emitter.c
+++ lab2/lab2_B/emitter.c
@@ -6,15 +6,15 @@ void emit (int token_type, int token_value)  /*  generates output  */
 {
     switch(token_type) {
         case '+' : case '-' : case '*' : case '/' : case '=' :
-        printf("%c", token_type); break;
+        printf("%c\n", token_type); break;
     case DIV:
         printf("DIV\n"); break; 
     case MOD:
         printf("MOD\n"); break;
     case NUM:
-        printf("%d", token_value); break;
+        printf("%d\n", token_value); break;
     case ID:
-        printf("%s", symtable[token_value].lexeme); break; 
+        printf("%s\n", symtable[token_value].lexeme); break; 
     default:     
         printf("[Unknown token %d, with value %d]\n", token_type, token_value);
     }
