f89022a836fd434a170ccee1d28053cb40adeb4a
Anton S <soderlund.anton@gmail.com>
2020-01-09 04:31:12 +0100

    lab6 done

---
+++ lab6/global.hpp
@@ -0,0 +1,55 @@
+/* global.h */
+
+#include <stdio.h>  /* include declarations for i/o routines */
+#include <ctype.h>  /* ... and for character test routines */
+#include <stdlib.h> /* ... and for some standard routines, such as exit */
+#include <string.h> /* ... and for string routines */
+#include "parser.tab.hpp"
+#define MAX_ID_LENGTH  128  /* for the buffer size */
+#define MAX_ARGS 3
+
+#define NONE   -1
+#define EOS    '\0'
+
+/*
+#define NUM    256
+#define DIV    257
+#define MOD    258
+#define ID     259
+#define DONE   260
+*/
+
+extern int token_value;   /*  value of token attribute */  
+extern int lineno;
+
+struct symentry {  /*  form of symbol table entry  */
+    char *lexeme; 
+    int  token_type;    
+    int value;		/*	för att lagra värden */
+};
+
+struct TreeNode{
+	int type;
+	int leaf_value;
+	TreeNode* args[MAX_ARGS];
+};
+
+typedef struct TreeNode TreeNode;
+
+extern TreeNode* mkleaf(int type, int value);
+extern TreeNode* mknode(int type, TreeNode* a0 = 0, TreeNode* a1 = 0, TreeNode* a2 = 0);
+
+extern int treeExec(TreeNode *p);
+extern int calc(int type, int op1, int op2);
+
+extern struct symentry symtable[];  /* symbol table  */
+
+extern void init();  /*  loads keywords into symtable  */
+extern void error(const char* message);  /*  generates all error messages  */
+extern int lexan();  /*  lexical analyzer  */
+extern void parse();  /*  parses and translates expression list  */
+extern int insert(char *s, int token_type);    /*  returns position of entry for s */
+extern int lookup(char *s);         /* returns position of entry for s, or -1 if not found */
+extern void emit (int token_type, int token_value);  /*  generates output  */
+extern void push(int token_type, int value);
+extern int pop();
---
+++ lab6/treeExec.cpp
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <iostream>
+#include "global.hpp"
+
+int calc(int type, int op1, int op2){
+	printf("Calculating %d %c %d\n",op1,type,op2);
+	switch(type){
+		case '+':
+			return op1 + op2;
+		case '-':
+			return op1 - op2;
+		case '/':
+			return op1/op2;
+		case '*':
+			return op1*op2;
+		case '%':
+			return op1%op2;
+		case '<':
+			return op1<op2;
+		case '>':
+			return op1>op2;
+		case '&':
+			return op1&op2;
+		case '|':
+			return op1|op2;
+		default:
+			return 0; 
+	}
+}
+
+
+int treeExec(TreeNode *p){
+	if(p == 0){
+		return -1;
+	}
+
+	int nr_args = 0;
+	for(int i = 0; i < 3; i++){
+		if(p->args[i] != 0)
+			nr_args++;
+	}
+
+	switch(p->type){
+		case LIST:
+			treeExec(p->args[0]);
+			if(nr_args>1)
+				treeExec(p->args[1]); //Om andra noden existerar
+		case NUM:
+			return p->leaf_value;
+		case ID:
+			return symtable[p->leaf_value].value;
+		/* Operatorer som använder calc() */
+		case '+': case '-': case '/': case '*': case '%':
+		case '>': case '<': case '&': case '|':
+			return calc(p->type,treeExec(p->args[0]),treeExec(p->args[1]));
+		case '=':{
+			int equalto = treeExec(p->args[1]);
+			printf("Setting %s = %d\n", symtable[p->args[0]->leaf_value].lexeme, equalto);
+			symtable[p->args[0]->leaf_value].value = equalto;
+			break;
+		}
+		case IF:
+			if(treeExec(p->args[0])) //Om påståendet är sant, exekvera if-blocket
+				treeExec(p->args[1]);
+			else if(nr_args > 1)	//Om else-satsen existerar
+				treeExec(p->args[2]);
+			break;
+		case ELSE:
+			treeExec(p->args[0]); //Else-satsen block och vi kommer endast hit genom ovanstående else if sats
+		case WHILE:
+			while(treeExec(p->args[0])){
+				treeExec(p->args[1]); // Medans villkoret är sant, exekvera blocket
+			}
+			break;
+		case PRINT:
+			printf("%d\n",treeExec(p->args[0]));
+			break;
+		case READ:{
+			int n;
+			printf("Enter value for variable %s\n",symtable[p->args[0]->leaf_value].lexeme);
+			std::cin >> n;
+			symtable[p->args[0]->leaf_value].value = n;
+			break;
+		}
+	}
+}
